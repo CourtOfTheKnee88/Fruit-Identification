4868571b5e1c9459e4782c94bd0d4b954f95b983
Courtney Jackson <courtneymjackson8@gmail.com>
2024-12-02 08:52:17 -0500

    Update Fruit.py

--- Fruit.py
+++ Fruit.py
@@ -70,24 +70,59 @@ class FruitDataset(Dataset):
     
 # CNN model
 class FruitCNN(nn.Module):
-    def __init__(self):
-        super(FruitCNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
-        self.fc1 = nn.Linear(32 * 64 * 64, 120)  
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 3)  # 3 classes
+   def __init__(self, num_classes=5):
+    super(FruitCNN, self).__init__()
+    self.layer1 = nn.Sequential(
+        nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=0),
+        nn.BatchNorm2d(96),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size = 3, stride = 2))
+    self.layer2 = nn.Sequential(
+        nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),
+        nn.BatchNorm2d(256),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size = 3, stride = 2))
+    self.layer3 = nn.Sequential(
+        nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+        nn.BatchNorm2d(384),
+        nn.ReLU())
+    self.layer4 = nn.Sequential(
+        nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1),
+        nn.BatchNorm2d(384),
+        nn.ReLU())
+    self.layer5 = nn.Sequential(
+        nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+        nn.BatchNorm2d(256),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size = 3, stride = 2))
+    self.fc = nn.Sequential(
+        nn.Dropout(0.5),
+        nn.Linear(9216, 4096),
+        nn.ReLU())
+    self.fc1 = nn.Sequential(
+        nn.Dropout(0.5),
+        nn.Linear(4096, 4096),
+        nn.ReLU())
+    self.fc2= nn.Sequential(
+        nn.Linear(4096, num_classes))
 
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x,2,2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x,2,2)
-        x = x.view(-1, 32 * 64 * 64)  
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+def __init__(self):
+    super().__init__()
+    self.conv1 = nn.Conv2d(3, 6, 5)
+    self.pool = nn.MaxPool2d(2, 2)
+    self.conv2 = nn.Conv2d(6, 16, 5)
+    self.fc1 = nn.Linear(16 * 5 * 5, 120)
+    self.fc2 = nn.Linear(120, 84)
+    self.fc3 = nn.Linear(84, 10)
+
+def forward(self, x):
+    x = self.pool(F.relu(self.conv1(x)))
+    x = self.pool(F.relu(self.conv2(x)))
+    x = torch.flatten(x, 1) # flatten all dimensions except batch
+    x = F.relu(self.fc1(x))
+    x = F.relu(self.fc2(x))
+    x = self.fc3(x)
+    return x
        
 transform = transforms.Compose([
     transforms.Resize((200, 200)),  
