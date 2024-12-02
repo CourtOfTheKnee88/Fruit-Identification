3002a7abe036c1f8dde09e3945bfaabeaf50902e
Courtney Jackson <courtneymjackson8@gmail.com>
2024-12-02 08:46:31 -0500

    Update Fruit.py

--- Fruit.py
+++ Fruit.py
@@ -90,7 +90,7 @@ class FruitCNN(nn.Module):
         return x
        
 transform = transforms.Compose([
-    transforms.Resize((256, 256)),  
+    transforms.Resize((200, 200)),  
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
